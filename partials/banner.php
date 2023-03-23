c2f49c2e6c76c5631e7c45f463dcb98a2402305f
Mahmudul Hasam Masum <mhmasum231@gmail.com>
2023-03-23 16:07:45 +0600

    first commit

---
+++ partials/banner.php
@@ -0,0 +1,8 @@
+<div class="jumbotron jumbotron-fluid position-relative overlay-bottom" style="margin-bottom: 90px;">
+        <div class="container text-center my-5 py-5">
+            <h1 class="text-white mt-4 mb-4">Learn with effectively</h1>
+            <h3 class="text-white display-4 mb-5">Solve your academic problems with our experts</h1>
+            <div class="mx-auto mb-5" style="width: 100%; max-width: 600px;">
+            </div>
+        </div>
+    </div>
